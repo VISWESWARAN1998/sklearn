4458e75938b6d07db428e4325e2e140558dc6be9
unknown <visweswaran.nagasivam98@gmail.com>
2019-09-20 16:12:29 +0530

    Matrix class for code project :sparkling_heart:

--- sklearn/matrix.h
+++ sklearn/matrix.h
@@ -83,5 +83,143 @@ public:
 			}
 		}
 	}
+
+	void  getCofactor(std::vector<std::vector<T>> matrix, std::vector<std::vector<T>> &temp, int p, int q, int n)
+	{
+		int i = 0, j = 0;
+		for (int row = 0; row < n; row++)
+		{
+			for (int col = 0; col < n; col++)
+			{
+				if (row != p && col != q)
+				{
+					temp[i][j++] = matrix[row][col];
+					if (j == n - 1)
+					{
+						j = 0;
+						i++;
+					}
+				}
+			}
+		}
+	}
+
+	void adjoint(std::vector<std::vector<T>> matrix, std::vector<std::vector<T>> &adj)
+	{
+		int matrixSize = matrix.size();
+
+		if (matrixSize == 1)
+		{
+			adj[0][0] = 1;
+		}
+
+		// temp is used to store cofactors of matrix[][] 
+		int sign = 1;
+
+		int number_of_elements = matrixSize;
+		//cout << number_of_elements;
+		int default_value = 1;
+		int number_of_rows = 0;
+
+		if (matrix.size() > 0)
+		{
+			number_of_rows = matrix[0].size();
+		}
+
+		std::vector<T> defaultValues(number_of_rows, default_value);
+		std::vector<std::vector<T>> temp(number_of_elements, defaultValues);
+
+
+		for (int i = 0; i<matrixSize; i++)
+		{
+			for (int j = 0; j<matrixSize; j++)
+			{
+				// Get cofactor of matrix[i][j] 
+				getCofactor(matrix, temp, i, j, matrixSize);
+
+				// sign of adj[j][i] positive if sum of row 
+				// and column indexes is even. 
+				sign = ((i + j) % 2 == 0) ? 1 : -1;
+
+				// Interchanging rows and columns to get the 
+				// transpose of the cofactor matrix 
+				adj[j][i] = (sign)*(determinantOfMatrix(temp, matrixSize - 1));
+			}
+		}
+
+	}
+
+	bool inverse(std::vector<std::vector<T>> matrix, std::vector<std::vector<T>> &inverse)
+	{
+
+		//std::vector<std::vector<T>> inverse;
+		int matrixSize = matrix.size();
+		// Find determinant of matrix[][] 
+		int det = determinantOfMatrix(matrix, matrixSize);
+		if (det == 0)
+		{
+			return false;
+		}
+
+		// Find adjoint 
+		int number_of_elements = matrix.size();
+		//cout << number_of_elements;
+		int default_value = 1;
+		int number_of_rows = 0;
+
+		if (matrix.size() > 0)
+		{
+			number_of_rows = matrix[0].size();
+		}
+
+		std::vector<T> defaultValues(number_of_rows, default_value);
+		std::vector<std::vector<T>> adj(number_of_elements, defaultValues);
+
+		adjoint(matrix, adj);
+
+		// Find Inverse using formula "inverse(matrix) = adj(matrix)/det(matrix)" 
+		for (int i = 0; i<matrixSize; i++)
+			for (int j = 0; j<matrixSize; j++)
+				inverse[i][j] = adj[i][j] / double(det);
+
+		return true;
+
+	}
+
+	double determinantOfMatrix(std::vector<std::vector<T>> matrix, unsigned long int n)
+	{
+		double D = 0.0; 
+		if (n == 1)
+		{
+			D = matrix[0][0];
+			return D;
+		}
+
+		//int temp[matrixSize][matrixSize]; // To store cofactors 
+
+		int sign = 1; // To store sign multiplier 
+
+		int number_of_elements = matrix.size();
+		//cout << number_of_elements;
+		int default_value = 1;
+		int number_of_rows = 0;
+
+		if (matrix.size() > 0)
+		{
+			number_of_rows = matrix[0].size();
+		}
+
+		std::vector<T> defaultValues(number_of_rows, default_value);
+		std::vector<std::vector<T>> temp(number_of_elements, defaultValues);
+
+		for (int f = 0; f < n; f++)
+		{
+			getCofactor(matrix, temp, 0, f, n);
+
+			D += sign * matrix[0][f] * determinantOfMatrix(temp, n - 1);
+			sign = -sign;
+		}
+		return D;
+	}
 };
 
