242fcbb374a6affdd4c25dd829a830d04eeab124
unknown <visweswaran.nagasivam98@gmail.com>
2020-03-14 19:29:17 +0530

    CV

--- sklearn/count_vectorizer.cpp
+++ sklearn/count_vectorizer.cpp
@@ -4,9 +4,19 @@
 
 
 template<typename T>
-count_vectorizer<T>::count_vectorizer(std::vector<T> &sentences)
+void count_vectorizer<T>::generate_encodings()
+{
+	for (unsigned long int i = 0; i < header_values.size(); i++)
+	{
+		encoded_values[header_values[i]] = i;
+	}
+}
+
+template<typename T>
+count_vectorizer<T>::count_vectorizer(std::vector<T> &sentences, unsigned long int max_features)
 {
 	this->sentences = sentences;
+	this->max_features = max_features;
 }
 
 template<typename T>
@@ -56,13 +66,64 @@ void count_vectorizer<T>::generate_tokens_array()
 }
 
 template<typename T>
+void count_vectorizer<T>::generate_headers()
+{
+	if (max_features >= tokens.size())
+	{
+		for (auto token : tokens)
+		{
+			header_values.push_back(token.first);
+		}
+		std::sort(header_values.begin(), header_values.end());
+		return;
+	}
+	while (tokens.size() > max_features)
+	{
+		for (unsigned long int min : max_values)
+		{
+			std::vector<T> matched_values;
+			for (auto itr : tokens)
+			{
+				if (itr.second == min)
+				{
+					matched_values.push_back(itr.first);
+				}
+			}
+			std::sort(matched_values.begin(), matched_values.end());
+			if (matched_values.size() > 0)
+			{
+				// delete the last matched values
+				tokens.erase(matched_values[matched_values.size() - 1]);
+				break;
+			}
+		}
+	}
+
+	for (auto token : tokens)
+	{
+		header_values.push_back(token.first);
+	}
+	std::sort(header_values.begin(), header_values.end());
+}
+
+template<typename T>
 void count_vectorizer<T>::get_array()
 {
 	generate_tokens_array();
-	for (auto i : max_values)
-	{
-		std::cout << i << "\n";
-	}
+	generate_headers();
+	generate_encodings();
+}
+
+template<typename T>
+std::vector<T> count_vectorizer<T>::get_headers()
+{
+	return header_values;
+}
+
+template<typename T>
+std::map<T, unsigned long int> count_vectorizer<T>::encodings()
+{
+	return encoded_values;
 }
 
 
--- sklearn/count_vectorizer.h
+++ sklearn/count_vectorizer.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <algorithm>
 
 template <typename T>
 class count_vectorizer
@@ -13,10 +14,21 @@ private:
 	std::vector<T> sentences;
 	std::set<unsigned long int> max_values;
 	std::map<T, unsigned long int> tokens;
+	unsigned long int max_features; // Total no of output features
+	std::vector<T> header_values;
+	std::map<T, unsigned long int> encoded_values;
 
 	// Split all the sentences into unique tokens
 	void generate_tokens_array();
+
+	// Generate the headers
+	void generate_headers();
+
+	// Generate the encodings
+	void generate_encodings();
 public:
-	count_vectorizer(std::vector<T> &sentences);
+	count_vectorizer(std::vector<T> &sentences, unsigned long int max_features);
 	void get_array();
+	std::vector<T> get_headers();
+	std::map<T, unsigned long int> encodings();
 };
